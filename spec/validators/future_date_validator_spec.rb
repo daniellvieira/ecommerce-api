5997bce9df99d738996d900186088f548c7534a5
Daniel Vieira <danielvieiraufc@gmail.com>
2020-11-23 14:42:35 -0300

    Criando uma Validação Customizada - Parte 2

--- spec/validators/future_date_validator_spec.rb
+++ spec/validators/future_date_validator_spec.rb
@@ -13,18 +13,24 @@ describe FutureDateValidator, type: :validators do
 
   context 'when date is before current date' do
     before { subject.date = 1.day.ago }
-    it 'should be invalid' do
-      expect(subject.valid?).to be_falsey
-    end
+    it { expect(subject.valid?).to be_falsey }
     it 'adds an error on model' do
       subject.valid?
       expect(subject.errors.keys).to include(:date)
     end
   end
+
   context 'when date is equal current date' do
-
+    before { subject.date = Time.zone.now }
+    it { expect(subject.valid?).to be_falsey }
+    it 'adds an error on model' do
+      subject.valid?
+      expect(subject.errors.keys).to include(:date)
+    end
   end
-  context 'when date is greater than current date' do
 
+  context 'when date is greater than current date' do
+    before { subject.date = Time.zone.now + 1.day }
+    it { expect(subject.valid?).to be_truthy }
   end
 end
