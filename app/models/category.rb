3c5cc66b6c71b93ca6040beed88e282a77d2f796
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2020-11-27 21:01:54 -0300

    Adicionando Busca por nome nos Models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ApplicationRecord
+  include NameSearchable
+
   # Relationships
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
   # Validations
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, 
+                   uniqueness: { case_sensitive: false }
 end
--- spec/models/product_category_spec.rb
+++ spec/models/product_category_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe ProductCategory, type: :model do
   # Relationships
   it { expect(subject).to belong_to :product }
   it { expect(subject).to belong_to :category }
+
+  it_behaves_like "name searchable concern", :category
 end
