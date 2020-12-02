c3dd77cec70cd7a26d5bb8ea9f69cadfdc641a0f
Daniel Vieira <danielvieiraufc@gmail.com>
2020-12-02 17:00:52 -0300

    Requisição com parâmetros de Busca e Ordenação e SimpleCOV

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,9 +1,9 @@
 module Admin::V1
   class CategoriesController < ApiController
-    before_action :load_category, only: %i[update destroy]
+    before_action :load_category, only: [:update, :destroy]
 
     def index
-      @categories = Category.all
+      @categories = load_categories
     end
 
     def create
@@ -29,9 +29,14 @@ module Admin::V1
       @category = Category.find(params[:id])
     end
 
+    def load_categories
+      permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
+      Admin::ModelLoadingService.new(Category.all, permitted).call
+    end
+
     def category_params
       return {} unless params.has_key?(:category)
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:id, :name)
     end
 
     def save_category!
